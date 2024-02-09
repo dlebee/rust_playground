c5558a884d97559619fd84b7c92e242012a73320
David Lebee (Personal) <Personal@DavidLebees-MacBook-Pro.local>
2024-02-09 10:50:37 -0500

    yep

--- eth_watcher_toy2/src/main.rs
+++ eth_watcher_toy2/src/main.rs
@@ -13,11 +13,7 @@ async fn main() -> Result<()> {
     let mut stream2 = provider2.subscribe_blocks().await?;
 
 
-
     loop {
-
-        // Spawn a task to handle user input
-        let input_handler = tokio::spawn(handle_user_input());
 
         tokio::select! {
             Some(block) = stream1.next() => {
@@ -27,38 +23,6 @@ async fn main() -> Result<()> {
             Some(block) = stream2.next() => {
                 println!("Creditcoin testnet - Ts: {:?}, block number: {} -> {:?}", block.timestamp, block.number.unwrap(), block.hash.unwrap());
             }
-            input = input_handler => {
-                match input {
-                    Ok(user_input) => {
-                        handle_user_command(user_input.unwrap());
-                    }
-                    Err(e) => eprintln!("Error reading user input: {}", e),
-                }
-            }
-        }
-    }
-}
-
-async fn handle_user_input() -> Result<String> {
-    let mut reader = tokio::io::BufReader::new(stdin());
-    let mut input = String::new();
-
-    // Read user input line by line
-    reader.read_line(&mut input).await?;
-
-    Ok(input)
-}
-
-fn handle_user_command(command: String) {
-    let sanitized_command = command.trim().to_lowercase();
-    
-    match sanitized_command.as_str() {
-        "exit" => {
-            println!("Exiting the program.");
-            std::process::exit(0);
-        }
-        _ => {
-            println!("Unknown command: {}", command);
         }
     }
 }
